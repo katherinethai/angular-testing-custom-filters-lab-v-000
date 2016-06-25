108e62ce1c8bb69247b9647139185288b2e7cc64
katherinethai <kthai1618@gmail.com>
2016-06-25 16:25:10 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,24 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+
+  it('only returns people whose favorite food is ice cream', function() {
+    var array = [{
+      name: 'Person 1',
+      favoriteFood: 'ice cream'
+    }, {
+      name: 'Person 2',
+      favoriteFood: 'cake'
+    },
+    {
+      name: 'Person 3',
+      favoriteFood: 'ice cream'
+    }];
+
+    var results = $filter('favoriteFood')(array, 'ice cream')
+
+    expect(results.length).toBe(2);
+    expect(results[0].name).toBe('Person 1');
+  }) 
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,11 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('removes all vowels from the string', function() {
+    var string = 'Hello world!';
+
+    var result = $filter('removeAllVowels')(string);
+
+    expect(result).toBe('Hll wrld!');
+  });
 });
